1f70af64a2d821cebfbb4e8f19c4a8173b51150f
friendlyguy774 <friendlyguy774@gmail.com>
2022-01-21 19:09:27 +0800

    block moves left, right and down

--- main.py
+++ main.py
@@ -1,94 +1,49 @@
 import pygame
+from pygame import K_LEFT, K_RIGHT, K_DOWN
+
+import square
 from sys import exit
 
 
 # scan all coordinates for blocks
 # if blocks exist, print out the box depending on colour
 def square_printing():
-    for y in range(0, 20):
-        for x in range(0, 10):
-            if square_coordinates[y][x] == 1:
-                square_rect[y][x] = (square_red_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_red_surfaces, square_rect[y][x])
-            if square_coordinates[y][x] == 2:
-                square_rect[y][x] = (square_green_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_green_surfaces, square_rect[y][x])
-            if square_coordinates[y][x] == 3:
-                square_rect[y][x] = (square_yellow_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_yellow_surfaces, square_rect[y][x])
-            if square_coordinates[y][x] == 4:
-                square_rect[y][x] = (square_orange_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_orange_surfaces, square_rect[y][x])
-            if square_coordinates[y][x] == 5:
-                square_rect[y][x] = (square_pink_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_pink_surfaces, square_rect[y][x])
-            if square_coordinates[y][x] == 6:
-                square_rect[y][x] = (square_blue_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_blue_surfaces, square_rect[y][x])
-            if square_coordinates[y][x] == 7:
-                square_rect[y][x] = (square_purple_surfaces.get_rect(topleft=(40 * x, 40 * y)))
-                screen.blit(square_purple_surfaces, square_rect[y][x])
-
-
-def block_generator():
-    if tetris == 1:
-        return [[0, 4], [0, 5], [1, 5], [1, 6]]
+    for y in range(20):
+        for x in range(10):
+            if square.square_coordinates[y][x] == 1:
+                square.square_rect[y][x] = (square_red_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_red_surfaces, square.square_rect[y][x])
+            if square.square_coordinates[y][x] == 2:
+                square.square_rect[y][x] = (square_green_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_green_surfaces, square.square_rect[y][x])
+            if square.square_coordinates[y][x] == 3:
+                square.square_rect[y][x] = (square_yellow_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_yellow_surfaces, square.square_rect[y][x])
+            if square.square_coordinates[y][x] == 4:
+                square.square_rect[y][x] = (square_orange_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_orange_surfaces, square.square_rect[y][x])
+            if square.square_coordinates[y][x] == 5:
+                square.square_rect[y][x] = (square_pink_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_pink_surfaces, square.square_rect[y][x])
+            if square.square_coordinates[y][x] == 6:
+                square.square_rect[y][x] = (square_blue_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_blue_surfaces, square.square_rect[y][x])
+            if square.square_coordinates[y][x] == 7:
+                square.square_rect[y][x] = (square_purple_surfaces.get_rect(topleft=(40 * x, 40 * y)))
+                screen.blit(square_purple_surfaces, square.square_rect[y][x])
 
 
 pygame.init()
-
 
 white = [255, 255, 255]
 
 screen = pygame.display.set_mode((400, 800))
 pygame.display.set_caption('Tetris')
 clock = pygame.time.Clock()
-counter = 1
+counter = 0
+game_running = True
 
-square_coordinates = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 2, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 3, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-                     [0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
-square_rect = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-tetris = 1
-moving_block = []
-store_block = []
-block_generate = True
+square = square.Square()
 
 square_red_surfaces = pygame.image.load('graphics/red.png').convert_alpha()  # red for Z_blocks, 1
 square_green_surfaces = pygame.image.load('graphics/green.png').convert_alpha()  # green for S_blocks, 2
@@ -98,13 +53,11 @@ square_pink_surfaces = pygame.image.load('graphics/pink.png').convert_alpha()  #
 square_blue_surfaces = pygame.image.load('graphics/blue.png').convert_alpha()  # blue for I-blocks, 6
 square_purple_surfaces = pygame.image.load('graphics/purple.png').convert_alpha()  # purple for T_blocks, 7
 
-
-def move_down(mb):
-    store = mb
-    for j in range(4):
-        mb[j][0] += 1
-    return store, mb
-
+# flow of the game
+# generate block
+# scan if block that is generated is already filled, if so then lose game
+# scan if bottom is filled or is beyond the game (y-axis >19), if so leave the block there and generate_block
+# move block down
 
 while True:
     for event in pygame.event.get():
@@ -112,28 +65,31 @@ while True:
             pygame.quit()
             exit()
 
-    # generate a tetris
-    if block_generate:
-        moving_block = block_generator()
-        for i in range(4):
-            square_coordinates[moving_block[i][0]][moving_block[i][1]] = tetris
-        square_printing()
-        block_generate = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_LEFT:
+                square.move_block_left()
+            if event.key == K_RIGHT:
+                square.move_block_right()
+            if event.key == K_DOWN:
+                square.move_block_down()
 
-    if counter == 100:
-        # new moving_block coordinates when block moves down are inputted and old block_coordinates are stored
-        store_block, moving_block = move_down(moving_block)
-        print(store_block, moving_block)
-        # for all store_block values, equate them to 0 to delete them
-        for i in range(4):
-            square_coordinates[store_block[i][0]][store_block[i][1]] = 0
-        print(square_coordinates)
+    if game_running:
         screen.fill(white)
-        for i in range(4):
-            square_coordinates[moving_block[i][0]][moving_block[i][1]] = tetris
+
+        if square.lose:
+            game_running = False
+            break
+
+        square.generate_block()
         square_printing()
-        counter = 1
-    else: counter += 1
+
+        if counter == 60:
+            square.update_moving_block()
+            counter = 1
+        else:
+            counter += 1
+        print(square.moving_block, square.store_block)
+        square.touch_down()
 
     pygame.display.update()
     pygame.time.Clock().tick(100)
--- square.py
+++ square.py
@@ -1,122 +1,253 @@
-from random import randint
-
-
 class Square:
     def __init__(self):
         # 3 lines to initiate shapes and 17 lines for display
-        self.square_coordinates = \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 3, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+        self.block_generate = None
+        self.square_coordinates = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-        self.square_rect = \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.square_rect = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0]]
 
-        self.tetris = 1
+        self.stationary = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-        self.moving_block1 = []
-        self.moving_block2 = []
-        self.moving_block3 = []
-        self.moving_block4 = []
+        self.tetris = 7
+        self.moving_block = [[0, 0], [0, 0], [0, 0], [0, 0]]
+        self.store_block = [[0, 0], [0, 0], [0, 0], [0, 0]]
+        self.block_generate = True
+        self.touch_floor = False
+        self.lose = False
+        self.move_down = True
 
-        self.store_block1 = []
-        self.store_block2 = []
-        self.store_block3 = []
-        self.store_block4 = []
+    def update_moving_block(self):
 
-        self.block_generator = True
+        temporary_storage = [0, 0, 0, 0]
 
-        self.pause = 1
+        for a in range(4):
+            temporary_storage[a] = [x for x in self.moving_block[a]]
 
-        self.change = 1
+        for y in temporary_storage:
+            y[0] += 1
 
-    def moving_block(self):
-        #  generate a random tetris depending on number(1,7)
-        if self.block_generator:
-            self.block_generate()
-            self.block_generator = False
+        for z in temporary_storage:
+            if z[0] < 20:
+                if self.stationary[z[0]][z[1]]:
+                    self.move_down = False
+                    self.touch_floor = True
+            if z[0] == 20:
+                self.move_down = False
+                self.touch_floor = True
 
-        self.render_block()  # render block
-        if self.pause == 120:  # wait 2 second
+        if self.move_down:
+            self.temporary_store()
+            self.block_down()
+            self.delete_old_tetris()
+            self.render_new_tetris()
+        print(self.square_coordinates)
 
-            self.store_block()  # store old coordinate into memory
-            self.block_down()  # make new coordinate
-            print(self.moving_block_1)
-            print(self.store_block1)
-            self.delete_previous_block()  # delete old blocks
-            self.pause = 0  # render new blocks back to start of loop
+    def generate_block(self):
+        if self.block_generate:
+            # choose a random number
+            # 1 -> z, 2 -> s, 3 -> 0, 4 -> l, 5 -> j, 6 -> i, 7 -> t
+            if self.tetris == 1:
+                self.moving_block = [[0, 4], [0, 5], [1, 5], [1, 6]]
+            if self.tetris == 2:
+                self.moving_block = [[1, 4], [1, 5], [0, 5], [0, 6]]
+            if self.tetris == 3:
+                self.moving_block = [[0, 5], [0, 6], [1, 5], [1, 6]]
+            if self.tetris == 4:
+                self.moving_block = [[0, 5], [1, 5], [2, 5], [2, 6]]
+            if self.tetris == 5:
+                self.moving_block = [[0, 6], [1, 6], [2, 6], [2, 5]]
+            if self.tetris == 6:
+                self.moving_block = [[0, 4], [0, 5], [0, 6], [0, 7]]
+            if self.tetris == 7:
+                self.moving_block = [[0, 4], [0, 5], [0, 6], [1, 5]]
+            self.block_generate = False
+            self.move_down = True
+            for x in self.moving_block:
+                self.square_coordinates[x[0]][x[1]] = self.tetris
+        for x in self.moving_block:
+            if self.stationary[x[0]][x[1]]:
+                self.lose = True
 
-        else:
-            self.pause += 1
+    def temporary_store(self):
+        for z in range(4):
+            self.store_block[z] = [j for j in self.moving_block[z]]
 
-
-    def block_generate(self):
-        if self.tetris == 1:
-            self.moving_block_1 = [0, 4]
-            self.moving_block_2 = [0, 5]
-            self.moving_block_3 = [1, 5]
-            self.moving_block_4 = [1, 6]
-
-    # store block coordinate before moving it down
-    def store_block(self):
-        self.store_block1 = self.moving_block_1
-        self.store_block2 = self.moving_block_2
-        self.store_block3 = self.moving_block_3
-        self.store_block4 = self.moving_block_4
-
-    #  make specific coordinate 1
-    def render_block(self):
-        self.square_coordinates[self.moving_block_1[0]][self.moving_block_1[1]] = self.tetris
-        self.square_coordinates[self.moving_block_2[0]][self.moving_block_2[1]] = self.tetris
-        self.square_coordinates[self.moving_block_3[0]][self.moving_block_3[1]] = self.tetris
-        self.square_coordinates[self.moving_block_4[0]][self.moving_block_4[1]] = self.tetris
-
-    # new coordinate is below by one
     def block_down(self):
-        self.moving_block_1[0] = self.moving_block_1[0] + 1
-        self.moving_block_2[0] = self.moving_block_2[0] + 1
-        self.moving_block_3[0] = self.moving_block_3[0] + 1
-        self.moving_block_4[0] = self.moving_block_4[0] + 1
 
-    # delete the block
-    def delete_previous_block(self):
-        self.square_coordinates[self.store_block1[0]][self.store_block1[1]] = 0
-        self.square_coordinates[self.store_block2[0]][self.store_block2[1]] = 0
-        self.square_coordinates[self.store_block3[0]][self.store_block3[1]] = 0
-        self.square_coordinates[self.store_block4[0]][self.store_block4[1]] = 0
+        for x in self.moving_block:
+            x[0] += 1
+
+    def touch_down(self):
+        if self.touch_floor:
+            for x in self.moving_block:
+                self.square_coordinates[x[0]][x[1]] = self.tetris
+                self.stationary[x[0]][x[1]] = 1
+                self.block_generate = True
+                self.touch_floor = False
+
+    def delete_old_tetris(self):
+        for x in self.store_block:
+            self.square_coordinates[x[0]][x[1]] = 0
+
+    def render_new_tetris(self):
+        for y in self.moving_block:
+            self.square_coordinates[y[0]][y[1]] = self.tetris
+
+    def move_block_left(self):
+        # create a temporary block that moved left
+        # check if touch side
+        # check if touch stationary
+        # if never touch side or touch stationary, able to move left
+        # store in temporary store
+        # move the block left
+        # delete before block
+        # paste the new block
+
+        created_temporary_block = [0, 0, 0, 0]
+        move_left = True
+        for a in range(4):
+            created_temporary_block[a] = [b for b in self.moving_block[a]]
+
+        for c in created_temporary_block:
+            c[1] -= 1
+
+        for z in created_temporary_block:
+            if self.stationary[z[0]][z[1]] or z[1] < 0:
+                move_left = False
+
+        if move_left:
+            self.temporary_store()
+
+            for x in self.moving_block:
+                x[1] -= 1
+
+            self.delete_old_tetris()
+
+            self.render_new_tetris()
+
+    def move_block_right(self):
+        # create a temporary block that moved left
+        # check if touch side
+        # check if touch stationary
+        # if never touch side or touch stationary, able to move left
+        # store in temporary store
+        # move the block left
+        # delete before block
+        # paste the new block
+
+        created_temporary_block = [0, 0, 0, 0]
+        move_right = True
+        for a in range(4):
+            created_temporary_block[a] = [b for b in self.moving_block[a]]
+
+        for c in created_temporary_block:
+            c[1] += 1
+
+        for z in created_temporary_block:
+            if z[1] < 9:
+                if self.stationary[z[0]][z[1]]:
+                    move_right = False
+            if z[1] == 10:
+                move_right = False
+
+        if move_right:
+            self.temporary_store()
+
+            for x in self.moving_block:
+                x[1] += 1
+
+            self.delete_old_tetris()
+
+            self.render_new_tetris()
+
+    def move_block_down(self):
+        # create a temporary block that moved left
+        # check if touch side
+        # check if touch stationary
+        # if never touch side or touch stationary, able to move left
+        # store in temporary store
+        # move the block left
+        # delete before block
+        # paste the new block
+
+        created_temporary_block = [0, 0, 0, 0]
+        move_down = True
+        for a in range(4):
+            created_temporary_block[a] = [b for b in self.moving_block[a]]
+
+        for c in created_temporary_block:
+            c[0] += 1
+
+        for z in created_temporary_block:
+            if z[0] < 20:
+                if self.stationary[z[0]][z[1]]:
+                    move_down = False
+            if z[0] == 20:
+                move_down = False
+
+        if move_down:
+            self.temporary_store()
+
+            for x in self.moving_block:
+                x[0] += 1
+
+            self.delete_old_tetris()
+
+            self.render_new_tetris()
